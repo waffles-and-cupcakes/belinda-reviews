cec4a28c92316afa5c1415597c80ce902527b6a1
Belinda <belindamomo@gmail.com>
2018-04-26 17:23:49 -0700

    edit for deploy

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -20,7 +20,7 @@ class App extends React.Component {
 
   componentDidMount() {
     var roomNum = window.location.pathname.split('/')[2];
-    axios.get(`http://13.56.58.150:3004/rooms/${roomNum}/reviews`)
+    axios.get(`http://reviews/rooms/${roomNum}/reviews`)
     .then((res) => {
       // console.log('RESPONSE: ', res);
       this.setState({
